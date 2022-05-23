b5de6b81790b6f26bc3108c658dfff400fc1491e
Saurabh Jindal <jindalsaurabh@gmail.com>
2022-05-23 17:36:59 +0200

    before GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,5 @@
 import "../styles/scss/style.scss";
 import Layout from "../layout/Layout";
-<<<<<<< HEAD
-import Script from "next/script";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-
-function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = url => {
-      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-        page_path: url,
-      })
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
-  return <Component {...pageProps} />
-=======
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -29,7 +7,6 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
     </Layout>
   );
->>>>>>> parent of b4dfcc4 (Google Analytics)
 }
 
-export default MyApp
+export default MyApp;
